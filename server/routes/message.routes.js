78196accdb72abc650fbae8c86bf29f7ee62c0da
pandeyaman26 <aipandey26@gmail.com>
2024-02-07 23:06:26 +0530

    change in message routes

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,6 +1,8 @@
 import express  from "express";
-import sendMessage from "../controllers/message.controller";
+import {sendMessage} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.post("/send/:id", sendMessage)
+
+export default router;
