5642f4bed8b54837c110ddf7b8ed54d6e25b2e65
pandeyaman26 <aipandey26@gmail.com>
2024-02-11 12:33:57 +0530

    adding a getMessage route

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -36,3 +36,12 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({error:"Internal server error!"})
     }
 }
+
+export const getMessages = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in getMessages controller",error);
+        res.status(500).json({error:"Internal server error!"});
+    }
+}
--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -4,6 +4,7 @@ import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
