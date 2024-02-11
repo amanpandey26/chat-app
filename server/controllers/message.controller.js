2dc3acb0dcc99e34f24e2be490c15b755aa6bc02
pandeyaman26 <aipandey26@gmail.com>
2024-02-11 12:27:23 +0530

    added Promise to save changes in message controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -27,8 +27,7 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        await conversation.save();
-        await newMessage.save();
+        await Promise.all([conversation.save(), newMessage.save()])
 
         res.status(201).json(newMessage);
 
