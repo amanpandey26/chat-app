e32ad86866cff227f5c6353a22be55a235236e75
pandeyaman26 <aipandey26@gmail.com>
2024-02-14 20:11:56 +0530

    SignUp component complete

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import GenderCheckbox from "./GenderCheckbox";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="flex flrx-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          SignUp <span className="text-pink-300">WeChat</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Full Name"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Your Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          
+          <GenderCheckbox />
 
-export default SignUp
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-pink-300 mt-2 inline-block"
+          >
+            Already have an account?
+          </a>
+          <div>
+          <button className="btn btn-block btn-sm mt-2">SignUp</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
