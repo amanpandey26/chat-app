76589ac598db0671cb54206efe5fd2ef044b182a
pandeyaman26 <aipandey26@gmail.com>
2024-02-16 19:10:52 +0530

    chanegs in scrollbar for messages container

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import Messages from "./Messages"
 
 const MessageContainer = () => {
   return (
@@ -10,7 +11,7 @@ const MessageContainer = () => {
 
 
             <Messages />
-            <MessagesInput />
+            {/* <MessagesInput /> */}
         </>
     </div>
   )
