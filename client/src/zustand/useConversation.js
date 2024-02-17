d4955c218bc1711ad2affd7ea797b49869c35c57
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 01:29:00 +0530

    changes in Conversation to set background color for selected chat

--- client/src/zustand/useConversation.js
+++ client/src/zustand/useConversation.js
@@ -2,7 +2,7 @@ import {create} from "zustand"
 
 const useConversation = create((set)=> ({
     selectedConversation: null,
-    setSelectedConversation: (selectedConversation)=> selectedConversation({selectedConversation}),
+    setSelectedConversation: (selectedConversation)=> set({selectedConversation}),
     messages:[],
     setMessages: (messages) => set({messages})
 }))
