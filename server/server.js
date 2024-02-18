c94bc04933b9dafa81378d592b4f5c851d931d87
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 17:44:12 +0530

    used path from node and __dirname along with a wildcard get method to return homepage

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000; 
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // to parse the incoming reqs with JSON (from req.body)
 app.use(cookieParser());
 
@@ -20,8 +23,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-app.get("/",(req,res) => {
-    res.send("home directory ...")    
+app.use(express.static(path.join(__dirname,"/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
