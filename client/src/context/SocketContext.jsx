44859a0fc907110fdd933816dcf31518032e4f22
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 16:41:43 +0530

    SocketContext completed

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,10 +1,44 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client"
 
-export const SocketContext = createContext();
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext);
+}
 
 export const SocketContextProvider = ({children}) => {
+    const [socket, setSocket] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState(null);
+    const {authUser} = useAuthContext();
+
+    useEffect(()=> {
+        if (authUser) {
+            const socket = io("http://localhost:8000", {
+                query: {
+                    userId: authUser._id
+                }
+            });
+            setSocket(socket);
+
+            socket.on("getOnlineUsers", (users) => {
+                setOnlineUsers(users);
+            })
+
+
+
+            return () => socket.close();
+        } else {
+            if (socket) {
+                socket.close();
+                setSocket(null);
+            }
+        }
+    },[authUser])
+
     return (
-        <SocketContext.Provider value={{}}>
+        <SocketContext.Provider value={{socket, onlineUsers}}>
             {children}
         </SocketContext.Provider>
     )
