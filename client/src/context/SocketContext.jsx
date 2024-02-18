4f328253484f4e4735d5138eff17c1a23cc956de
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 20:28:46 +0530

    socket url changed to render

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(()=> {
         if (authUser) {
-            const socket = io("https://wechat-alpha-six.vercel.app", {
+            const socket = io("https://wechat-foyo.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
