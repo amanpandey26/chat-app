9a6652dc501b0e62dd74cdfa8445464058232f41
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 11:56:32 +0530

    MessageSkeleton created and integrated in Messages

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,23 +1,18 @@
 // import React from 'react'
 import useGetMessages from "../../hooks/useGetMessages"
 import Message from "./Message"
+import MessageSkeleton from "../skeletons/MessageSkeleton"
 
 const Messages = () => {
   const {loading, messages} = useGetMessages();
   console.log("messages:",messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+       {loading && [...Array(3)].map((_,index)=><MessageSkeleton key={index}/>)}
+
+       {!loading && messages.length===0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+       )}
     </div>
   )
 }
