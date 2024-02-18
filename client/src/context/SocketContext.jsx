9af383d4997045161e9c4ccd7fb72b3ddc9d0014
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 20:26:26 +0530

    changes in socket url content

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(()=> {
         if (authUser) {
-            const socket = io("https://wechat-alpha-six.vercel.app/", {
+            const socket = io("https://wechat-alpha-six.vercel.app", {
                 query: {
                     userId: authUser._id
                 }
