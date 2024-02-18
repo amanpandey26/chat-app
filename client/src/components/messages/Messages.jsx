b419c19af3c388d71a9067321804e192723efc69
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 15:01:14 +0530

    Message and Messages updated with data integration

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -2,12 +2,29 @@
 import useGetMessages from "../../hooks/useGetMessages"
 import Message from "./Message"
 import MessageSkeleton from "../skeletons/MessageSkeleton"
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
   const {loading, messages} = useGetMessages();
-  console.log("messages:",messages);
+  const lastMessageRef = useRef();
+
+  useEffect(()=>{
+
+    setTimeout(()=>{
+      lastMessageRef.current?.scrollIntoView({behavior:"smooth"})
+    },100)
+    
+  },[messages])
+
   return (
     <div className="px-4 flex-1 overflow-auto">
+
+      {!loading && messages.length > 0 && messages.map((message)=> (
+        <div key={message._id} ref={lastMessageRef}>
+        <Message  message={message}/>
+        </div>
+      ))}
+
        {loading && [...Array(3)].map((_,index)=><MessageSkeleton key={index}/>)}
 
        {!loading && messages.length===0 && (
