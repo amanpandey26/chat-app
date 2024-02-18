716c0cdf3a2eed2d98e530c6ad1635befb85570a
pandeyaman26 <aipandey26@gmail.com>
2024-02-18 16:18:34 +0530

    changes in socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import http from "http"
+import http from "http";
 
 import express from "express";
 
@@ -7,18 +7,27 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:3000"],
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log("a user connected: ", socket.id);
+const userSocketMap = {}; // {userId : socketId}
 
-    socket.on("disconnect", () => {
-        console.log("user disconnected: ", socket.id);
-    })
-})
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
 
-export {app, io, server}
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected: ", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, io, server };
